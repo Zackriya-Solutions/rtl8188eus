71d923c94018a5bf81ef9de7681c9739b51fc677
Sujith S <sujithx007@gmail.com>
2022-01-14 12:52:49 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,12 @@
 echo 'blacklist r8188eu'|sudo tee -a '/etc/modprobe.d/realtek.conf'
-apt install dkms
+apt install dkms -Y
 chmod +x dkms-install.sh
 ./dkms-install.sh
 insmod 8188eu.ko
+wget -O - https://www.kismetwireless.net/repos/kismet-release.gpg.key | sudo apt-key add -
+echo "deb https://www.kismetwireless.net/repos/apt/release/$(lsb_release -cs) $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/kismet.list
+apt-get update
+apt-get install kismet
+usermod -aG kismet pi
+
 reboot
